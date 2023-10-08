4cbf1ed72e0605bd22d4be8b44b117a47371ef9a
Alex Auch <alexsamuelauch@gmail.com>
2023-10-08 12:22:14 -0600

    added user login endpoint

--- Entity/Backend/routes/api/index.js
+++ Entity/Backend/routes/api/index.js
@@ -1,35 +1,17 @@
 const router = require('express').Router();
+const sessionRouter = require('./session.js')
+const usersRouter = require('./users.js')
 const { restoreUser } = require('../../utils/auth.js')
 
 router.use(restoreUser)
 
-const { requireAuth } = require('../../utils/auth.js');
-router.get(
-    '/require-auth',
-    requireAuth,
-    (req, res) => {
-        return res.json(req.user);
-    }
-);
+router.use('/session', sessionRouter)
 
-router.get('/restore-user', (req, res) => {
-    return res.json(req.user)
-})
+router.use('/users', usersRouter)
 
 router.post('/test', (req, res) => {
-    res.json({ requestBody: req.body })
-})
-
-const { setTokenCookie } = require('../../utils/auth.js');
-const { User } = require('../../db/models');
-router.get('/set-token-cookie', async (req, res) => {
-    const user = await User.findOne({
-        where: {
-            username: 'Demo-lition'
-        }
-    });
-    setTokenCookie(res, user);
-    return res.json({ user });
+    res.json({ requestBody: req.body });
 });
+
 
 module.exports = router;
