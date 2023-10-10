5f2dcff3ddc0e9b3fa657d53559c74afc8f451f5
Alex Auch <alexsamuelauch@gmail.com>
2023-10-10 10:51:45 -0600

    modfied session reducer

--- Entity/frontend/src/store/csrf.js
+++ Entity/frontend/src/store/csrf.js
@@ -26,6 +26,6 @@ export async function csrfFetch(url, options = {}) {
     return res;
 }
 
-export const restoreCSRF = () => {
+export function restoreCSRF() {
     return csrfFetch('/api/csrf/restore')
 }
